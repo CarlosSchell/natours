78e444996f627f2cd6bb08fd40019c300bb4636e
CarlosSchell <carlos.schellenberger@gmail.com>
2021-01-07 12:59:51 -0300

    init

---
+++ after-section-06/controllers/tourController.js
@@ -0,0 +1,108 @@
+import fs from 'fs'
+import path from 'path'
+
+const __dirname = path.resolve()
+console.log(__dirname)
+// const __dirname = '.'
+console.log(`${__dirname}/dev-data/data/tours-simple.json`)
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  // fs.readFileSync('C:/Users/Carlos/Desktop/crash-courses/Node-Smetdmann/4-natours/after-section-06/dev-data/data/tours-simple.json')
+)
+
+const checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`)
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
+  next()
+}
+
+const checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price'
+    })
+  }
+  next()
+}
+
+const getAllTours = (req, res) => {
+  console.log(req.requestTime)
+
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours
+    }
+  })
+}
+
+const getTour = (req, res) => {
+  console.log(req.params)
+  const id = req.params.id * 1
+
+  const tour = tours.find(el => el.id === id)
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  })
+}
+
+const createTour = (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+
+  tours.push(newTour)
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      })
+    }
+  )
+}
+
+const updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  })
+}
+
+const deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+}
+
+export {
+  checkID,
+  checkBody,
+  getAllTours,
+  getTour,
+  createTour,
+  updateTour,
+  deleteTour
+}
---
+++ after-section-06/server.js
@@ -0,0 +1,12 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+import dotenv from 'dotenv'
+dotenv.config({ path: './config.env' })
+
+import app from './app.js'
+
+// console.log(process.env)
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`)
+})
